47257dd6968fdaa96fc013ac15a0eba976cb543a
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-14 09:45:23 +0000

    [Modify]README.md完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
+  # homes_controllerについてのルーティング
   root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # books_controllerについてのルーティング
   resources :books, :only => [:index, :create, :edit, :show, :update, :destroy]
-  # get '/books' => 'books#new'
 end
